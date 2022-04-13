2f80b0d51139947937da775e94708309207c44c0
BanditTech <thebbandit@gmail.com>
2022-04-12 21:23:57 -0700

    Initial Commit

---
+++ AutoGossip/AutoGossip.lua
@@ -0,0 +1,17 @@
+GossipFrame:HookScript("OnShow",function()
+	local optionNum = GetNumGossipOptions()
+	if (optionNum == 1) then
+		local gossipText = GetGossipOptions()
+		local noMod = not IsModifierKeyDown()
+		-- automate zone gossip like in the original addon
+		-- local zoneText = GetMinimapZoneText()
+		if (strfind(gossipText,"I would like to check my deposit box.")
+		or strfind(gossipText,"I need a ride.")
+		or strfind(gossipText,"Train me!")
+		or strfind(gossipText,"I would like training.")
+		or strfind(gossipText,"I want to browse your goods."))
+		and (noMod) then
+			SelectGossipOption(1)
+		end
+	end
+end)
